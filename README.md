45f79323945acb0687d196c711140e3cf0a84a6a
marwa mohammad <marwa.mohammad.nageeb@gmail.com>
2020-11-30 16:35:58 +0200

    second commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# lamp-by-js
---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+
+function turnOn()
+{
+
+    document.getElementById('myImage').src='images/pic_bulbon.gif';
+    document.getElementById('demo').style.backgroundColor="#09c";
+}
+
+function turnOff()
+{
+
+    document.getElementById('myImage').src='images/pic_bulboff.gif';
+    document.getElementById('demo').style.backgroundColor="#639";
+
+
+}
+
+
+
+
